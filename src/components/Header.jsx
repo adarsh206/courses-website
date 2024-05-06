b1b81fa6f68844ddfd762d225187d3b02a815cc0
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2024-05-06 18:35:39 +0530

    latest changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useState } from 'react'
 import { navLinks } from "../constants";
 
 const Header = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <header className='fixed flex items-center h-[70px] z-[1000] w-full border border-solid border-transparent'>
             <nav className='container flex items-center justify-between gap-x-16'>
-                <a href='/' className='text-2xl font-bold text-gray-10'>
-                    Digicourse
+                <a href='/' className='text-2xl font-bold text-fuchsia-600 '>
+                    DIGICOURSE
                 </a>
                 <div className='hidden lg:flex lg:justify-between w-full'>
                     <ul className='flex items-center gap-x-4'>
@@ -19,8 +21,24 @@ const Header = () => {
                             )
                         })}
                     </ul>
+                    <div className='flex items-center gap-x-4'>
+                        <button type="button" className='btn btn-outline'>
+                            Sign Up
+                        </button>
+                        <button type ="button"
+                        className='btn btn-primary'>Log In</button>
+                    </div>
+                </div>
+                <div className={`header__menu-icon flex lg:hidden relative w-5 h-4 flex-shrink-0 cursor-pointer overflow-hidden ${toggleMenu ? "active" : ""}`} 
+                onClick={() => setToggleMenu(!toggleMenu)}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
                 </div>
             </nav>
+            {/* --------- Mobile Navbar Menu --------- */ }
+            
+
     </header>
   )
 }
