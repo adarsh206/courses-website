502da07597415bff08dbcdd0db813df421042723
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2024-05-11 14:12:10 +0530

    latest changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 
 import './App.css'
+import CTA from './components/CTA'
+import Footer from './components/Footer'
 import Header from './components/Header'
 import Statistics from './components/Statistics'
 import About from './sections/About'
+import Blogs from './sections/Blogs'
+import Courses from './sections/Courses'
+import Guide from './sections/Guide'
 import Hero from './sections/Hero'
 import PopularCourses from './sections/PopularCourses'
+
 
 function App() {
   return (
@@ -14,6 +20,11 @@ function App() {
       <Statistics />
       <About />
       <PopularCourses />
+      <Courses />
+      <Guide />
+      <Blogs />
+      <CTA />
+      <Footer />
       
     </>
   )
